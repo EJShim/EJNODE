a68f9f5b6177b2188f8b8ac70428504a1ce12a35
ejshim <soulrommel@gmail.com>
2016-10-17 19:18:52 +0900

    test

--- core/assignment2/Manager.js
+++ core/assignment2/Manager.js
@@ -1,4 +1,4 @@
-//
+//adsds
 var E_Interactor = require('./E_Interactor.js');
 var E_Particle = require("../libs/physics/E_Particle.js");
 var E_FinitePlane = require("../libs/physics/E_FinitePlane.js");
