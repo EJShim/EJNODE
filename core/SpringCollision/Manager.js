bfb6ae7a6efe9c0be3805d6058afef7897d6d610
ejshim <soulrommel@gmail.com>
2016-10-18 22:23:37 +0900

    Add New Project

---
+++ core/SpringCollision/Manager.js
@@ -0,0 +1,280 @@
+var E_Interactor = require('./E_Interactor.js');
+var E_Particle = require("../libs/physics/E_Particle.js");
+var E_FinitePlane = require("../libs/physics/E_FinitePlane.js");
+var E_ParticleSystem = require("../libs/physics/E_ParticleSystem.js");
+
+function E_Manager()
+{
+  var canvas = document.getElementById("viewport");
+  var m_scene = new THREE.Scene();
+  var m_camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 100000 );
+  var m_renderer = new THREE.WebGLRenderer({canvas:canvas, preserveDrawingBuffer: true, antialias: true, alpha:true});
+  var m_interactor = new E_Interactor(this);
+  var m_particleSystem = new E_ParticleSystem(this);
+
+  var m_gravity = new THREE.Vector3(0.0, -10, 0.0);
+
+  this.thumbnailSaved = false;
+  this.starttime = new Date();
+
+  this.then = new Date();
+  this.interval = 1000 / 60;
+
+  //Time Step for Rendering
+  this.timeStep = 0;
+  this.prevTime = new Date();
+
+  this.groundMesh = [];
+
+  this.GetScene = function()
+  {
+    return m_scene;
+  }
+
+  this.GetCamera = function()
+  {
+    return m_camera;
+  }
+
+  this.GetRenderer = function()
+  {
+    return m_renderer;
+  }
+
+  this.GetInteractor = function()
+  {
+    return m_interactor;
+  }
+
+  this.GetGravity = function()
+  {
+    return m_gravity;
+  }
+
+  this.ParticleSystem = function()
+  {
+    return m_particleSystem;
+  }
+}
+
+E_Manager.prototype.Initialize = function()
+{
+  var renderer = this.GetRenderer();
+  //init renderrer
+  //renderer.setClearColor(0xffffff, 1);
+  renderer.setSize(window.innerWidth, window.innerHeight );
+
+  //Initialize Object
+  this.InitObject();
+}
+
+E_Manager.prototype.Animate = function()
+{
+  //Rendering
+  var now = new Date();
+  this.delta = (now - this.then);
+  if(this.delta > this.interval){
+    //Update Time Step
+    this.timeStep = (now - this.prevTime) / 1000;
+    if(this.timeStep > this.interval/1000) this.timeStep = this.interval / 1000;
+
+    this.ParticleSystem().Update();
+
+    var renderer = this.GetRenderer();
+    var camera = this.GetCamera();
+    var scene = this.GetScene();
+
+    renderer.render(scene, camera);
+
+    this.prevTime = now;
+    this.then = now - (this.delta % this.interval);
+    //this.then = now - (this.delta % this.interval);
+  }
+
+
+  //Update Interactor
+  var interactor = this.GetInteractor();
+  interactor.Update();
+
+  this.SaveThumbnail();
+
+  var Mgr = this;
+  requestAnimationFrame(Mgr.Animate.bind(Mgr));
+}
+
+
+E_Manager.prototype.InitObject = function()
+{
+  var scene = this.GetScene();
+  var camera = this.GetCamera();
+
+  camera.position.z = 3;
+  camera.position.y = 5;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  var pointLight = new THREE.PointLight({color:0xffffff});
+  pointLight.position.set(0, 5, 0);
+  var ambient = new THREE.AmbientLight({color:0x000000});
+
+  scene.add(pointLight);
+  scene.add(ambient);
+
+  this.GenerateRandomTriangle();
+
+}
+
+E_Manager.prototype.GenerateRandomTriangle = function()
+{
+  var scene = this.GetScene();
+  var system = this.ParticleSystem();
+
+  var scaleFactor = 1;
+  var vertices = [];
+  vertices[0] = new THREE.Vector3( -scaleFactor, -scaleFactor, -scaleFactor );
+  vertices[1] = new THREE.Vector3( -scaleFactor, -scaleFactor, scaleFactor );
+  vertices[2] = new THREE.Vector3( -scaleFactor, scaleFactor, -scaleFactor );
+  vertices[3] = new THREE.Vector3( scaleFactor, -scaleFactor, -scaleFactor );
+  vertices[4] = new THREE.Vector3( -scaleFactor, scaleFactor, scaleFactor );
+  vertices[5] = new THREE.Vector3( scaleFactor, -scaleFactor, scaleFactor );
+  vertices[6] = new THREE.Vector3( scaleFactor, scaleFactor, -scaleFactor );
+  vertices[7] = new THREE.Vector3( scaleFactor, scaleFactor, scaleFactor );
+
+
+  //ground
+    this.groundMesh[0] = new E_FinitePlane( vertices[0],vertices[1],vertices[2] );
+    this.groundMesh[1] = new E_FinitePlane( vertices[1],vertices[2],vertices[4] );
+    this.groundMesh[2] = new E_FinitePlane( vertices[0],vertices[1],vertices[5] );
+    this.groundMesh[3] = new E_FinitePlane( vertices[0],vertices[3],vertices[5] );
+    this.groundMesh[4] = new E_FinitePlane( vertices[0],vertices[2],vertices[6] );
+    this.groundMesh[5] = new E_FinitePlane( vertices[0],vertices[6],vertices[3] );
+    this.groundMesh[6] = new E_FinitePlane( vertices[1],vertices[4],vertices[7] );
+    this.groundMesh[7] = new E_FinitePlane( vertices[1],vertices[7],vertices[5] );
+    this.groundMesh[8] = new E_FinitePlane( vertices[3],vertices[5],vertices[6] );
+    this.groundMesh[9] = new E_FinitePlane( vertices[5],vertices[6],vertices[7] );
+
+
+
+  for(var i=0 ; i<10 ; i++){
+    this.groundMesh[i].material.transparent = true;
+    this.groundMesh[i].material.opacity = 0.2;
+    this.groundMesh[i].material.color = new THREE.Color(Math.random() / 4, Math.random() / 4, Math.random() / 4);
+
+    scene.add(this.groundMesh[i]);
+    system.add(this.groundMesh[i]);
+  }
+}
+
+E_Manager.prototype.ResetGround = function()
+{
+//   var v1 = new THREE.Vector3(this.frand(-2.5, -1.0), this.frand(-0.2, 0.2), this.frand(-2.5, -1.0));
+//   var v2 = new THREE.Vector3(this.frand(1.0, 2.5), this.frand(-0.2, 0.2) , this.frand(-2.5, -1.0));
+//   var v3 = new THREE.Vector3(this.frand(-0.5, -0.5), this.frand(-0.2, 0.2, this.frand(1.0, 2.5)));
+//
+//   this.groundMesh.geometry.vertices[0].set(v1.x, v1.y, v1.z) ;
+//   this.groundMesh.geometry.vertices[1].set(v2.x, v2.y, v2.z);
+//   this.groundMesh.geometry.vertices[2].set(v3.x, v3.y, v3.z);
+//   this.groundMesh.geometry.verticesNeedUpdate = true;
+}
+
+
+E_Manager.prototype.GenerateObjectScreen = function(x, y)
+{
+  var camera = this.GetCamera();
+
+
+  var raycaster = new THREE.Raycaster();
+  var mouse = new THREE.Vector2(x, y);
+
+  raycaster.setFromCamera(mouse, camera);
+
+  var distance = 5;
+  var generatePosition = new THREE.Vector3();
+  generatePosition.addVectors(raycaster.ray.origin.clone(), raycaster.ray.direction.clone().multiplyScalar(distance));
+
+  this.GenerateObject(generatePosition.x, generatePosition.y, generatePosition.z);
+
+}
+
+E_Manager.prototype.GenerateObject = function(x, y, z)
+{
+  var scene = this.GetScene();
+  var system = this.ParticleSystem();
+
+  var newMesh = new E_Particle(this, 0.1);
+  newMesh.position.set(x, y, z);
+  newMesh.material.color = new THREE.Color(Math.random() / 2, Math.random() / 2, Math.random() / 2);
+  newMesh.m_colorFixed = true;
+
+  var velFactor = 5;
+  newMesh.velocity.set( this.frand(-velFactor, velFactor), this.frand(-velFactor, velFactor), this.frand(-velFactor, velFactor) );
+
+  scene.add(newMesh);
+  system.add(newMesh);
+  // scene.add(newMesh2);
+  // scene.add(spring);
+
+  //Impulse
+  //newMesh.ApplyImpulse(raycaster.ray.direction.clone().normalize().multiplyScalar(0));
+
+
+  //log
+
+}
+
+E_Manager.prototype.SetLog = function(string)
+{
+  document.getElementById("log").innerHTML = string;
+}
+
+
+
+E_Manager.prototype.SaveThumbnail = function()
+{
+  var elapsedTime = (new Date() - this.starttime)/1000;
+  if(elapsedTime > 15 || this.thumbnailSaved){
+    return;
+  }
+
+
+  this.thumbnailSaved = true;
+  console.log("Thumbnail Saved");
+
+
+  if(this.thumbnailSaved) return;
+	//var testCanvas = m_renderer.domElement.toDataURL();
+	var canvasData = this.GetRenderer().domElement.toDataURL();
+	var postData = "canvasData="+canvasData;
+
+
+	//alert("canvasData ="+canvasData );
+	var ajax = new XMLHttpRequest();
+	ajax.open("POST",'../../thumbnail.php',true);
+	ajax.setRequestHeader('Content-Type', 'canvas/upload');
+	//ajax.setRequestHeader('Content-TypeLength', postData.length);
+
+
+	ajax.onreadystatechange=function()
+	{
+		if (ajax.readyState == 4)
+		{
+			//alert(ajax.responseText);
+			// Write out the filename.
+
+
+		}
+	}
+	ajax.send(postData);
+}
+
+E_Manager.prototype.frand = function(min, max)
+{
+  var range = max - min;
+  var value = Math.random();
+
+  value *= range;
+  value += min;
+
+  return value;
+}
+
+module.exports = E_Manager;
