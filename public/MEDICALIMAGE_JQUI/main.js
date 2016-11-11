5c288db8e8d89c142023754486ef4f20e11e36a2
EJ <EJ@EJs-iMac.local>
2016-11-11 13:20:13 +0900

    update

---
+++ public/MEDICALIMAGE_JQUI/main.js
@@ -0,0 +1,14 @@
+$( function() {
+  //
+  // $( "#e_mainView" ).resizable({
+  //   //containment: "#e_subView"
+  //   //alsoResize : "#e_subView"
+  // });
+
+
+  $( ".ec_subView" ).resizable({
+    //alsoResize : "#e_mainView"
+    minHeight: 150,
+    minWidth: 200
+  });
+} );
